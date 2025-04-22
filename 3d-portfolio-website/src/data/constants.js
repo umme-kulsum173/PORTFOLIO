ee2281bdcec54468519db9cc123a25b0e34e40f6
umme-kulsum173 <kumme0307@gmail.com>
2025-04-22 23:59:46 +0530

    Project added

---
+++ 3d-portfolio-website/src/data/constants.js
@@ -0,0 +1,199 @@
+import MernChat from "../images/MernChat.png";
+import Plant from "../images/Plant.png";
+import Expense from "../images/expense.webp"
+
+
+ 
+ export const Bio = {
+  name: "Umme Kulsum",
+  roles: [
+    "MERN Stack Developer",
+
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/umme-kulsum173",
+  resume:
+ " https://drive.google.com/file/d/1xZMDkN1zK_xce0SJtTVpqrx_Qg9hWGd6/view?usp=drive_link ",
+  linkedin:
+  "https://www.linkedin.com/in/umme-kulsum-b58ba7241?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",  
+  insta: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Tailwind css",
+       
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:"https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+      },
+     
+    
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+     
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+  
+  {
+    title: "Programming language",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/7/7f/C_Programming_Language_logo.svg",
+      },
+      
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+     
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "Don Bosco Institute of Technology, Bangalore",
+    date: " 2022 -  2026",
+    grade: "8.41 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Science and Engineering at Don Bosco Institute of Technology, Bangalore. I have completed 5 semesters and have a CGPA of 8.41. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Engineering - BE, Information Science and Engineering",
+  },
+  {
+    id: 1,
+    school: "Sarvothama PU college, Banglore",
+    date: " 2020 -  2022",
+    grade: "84.00%",
+    desc: "I completed my class 12 high school education at Sarvothama PU College, Bangalore, where I studied Science with  Maths and Biology .",
+    degree: "(XII), PCMB",
+  },
+  {
+    id: 2,
+    school: "Crescent High  School, Bangalore",
+    date: "Apr 2018 - Apr 2020",
+    grade: "80.0%",
+    desc: "I completed my class 10 education at Cresent High School, Bangalore.",
+    degree: "(X), ",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "MERN ChatApp with AI",
+    description:
+      "Built a full-stack real-time chat app using the MERN stack and Socket.IO for instant messaging. Integrated Google Gemini AI to generate smart, context-aware replies, enhancing user interactions. The frontend was developed with React and Context API to ensure a responsive and dynamic user experience.",
+    image:
+      MernChat,
+    // github: "",
+    
+  },
+  {
+    id: 1,
+    title: "Plsnt Analysis tool",
+    description:
+      "Developed a web application to help users analyze plant care requirements and determine ideal growth conditions. The frontend was built with React.js, and MongoDB was used for data management. It offers a smooth and informative experience for gardening enthusiasts.",
+    image: Plant,
+    
+    // github: "",
+    
+  },
+  {
+    id: 2,
+    title: "Expense Tracker",
+    description:
+      "Created an expense tracking app with React.js, Node.js, Firebase, and Auth.js. It allows users to manage their daily expenses in real time, offering secure login and a simple interface. Currently used by 20+ users for reliable financial tracking.",
+    image: Expense
+    // github: "",
+   
+   
+  },
+ 
+ ];
+
