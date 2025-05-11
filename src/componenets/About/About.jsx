2c5de8888b0c5df4f2ec3085e9142dffc302e4b2
umme-kulsum173 <kumme0307@gmail.com>
2025-05-12 00:27:50 +0530

    Fix: update RESUME BUTTON

--- src/componenets/About/About.jsx
+++ src/componenets/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1qD8cAC483rG1QURRb2Y-vfjrHq2j6SuH/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1qD8cAC483rG1QURRb2Y-vfjrHq2j6SuH/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
