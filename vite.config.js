f21fb3579f8cd066134b2074086f99d3dfd7e3fe
umme-kulsum173 <kumme0307@gmail.com>
2025-05-12 00:07:51 +0530

    Fix: update vite.config.js and image paths for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/port/',
+    base: '/',
   plugins: [
     tailwindcss(),
     react()],
